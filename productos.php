e6e5910639fbfc054e66df398052f06c75a81985
tolinware <tolinware@gmail.com>
2023-01-02 20:17:52 -0600

    se agregó una tabla

--- productos.php
+++ productos.php
@@ -7,6 +7,33 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>productos</h1>
+    <table>
+        <tr>
+            <th>N</th>
+            <th>M</th>
+            <th>Ñ</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>3</td>
+            <td>3</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>4</td>
+            <td>4</td>
+        </tr>
+    </table>
 </body>
 </html>
